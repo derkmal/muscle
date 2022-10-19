dc1d45079965981566f8520e671bb3f0932c5b02
darkmal <vagnerwillians@hotmail.com>
2022-10-18 22:15:50 -0300

    18-10 22:15hs

--- js/landingmuscle.js
+++ js/landingmuscle.js
@@ -1,18 +1,1 @@
 
-function myFunction(x) {
-    if(x.matches) {
-        let image = document.createElement('img');
-        image.src = "images/Muscle-1.png";
-        // image.style.opacity = 0.5;   
-        image.style.width = "50%";
-        image.style.display = "block";
-        image.style.margin = "0 auto";
-        document.querySelector("#myH3").insertAdjacentElement("beforeend", image);
-    } else {
-        document.querySelector("#myH3").removead("beforeend", image);
-    }
-}
-
-var x = window.matchMedia('(max-width: 700px)');
-myFunction(x);
-x.addEventListener(myFunction);
