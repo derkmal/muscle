78d932562285d06a853f378f53c97c4768b0b383
darkmal <vagnerwillians@hotmail.com>
2022-11-16 19:54:57 -0300

    last

--- js/landingmuscle.js
+++ js/landingmuscle.js
@@ -1,1 +1,13 @@
 
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="firstMessage">I will make you Front Dev</div>
+            <div>Mein Logo 4</div>
+        </header>
+    )
+}
+
+export default Header;
